950f86ef105bec34980b68b539389a973958f92a
vtak1908 <vukiet252@gmail.com>
2025-05-22 00:00:31 +0700

    Update login.php

--- Admin/login.php
+++ Admin/login.php
@@ -18,12 +18,12 @@ session_start(); ?>
 
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;700;900&display=swap" rel="stylesheet">
 
-        <link href="../User/css/bootstrap.min.css" rel="stylesheet">
-        <link href="../User/css/bootstrap-icons.css" rel="stylesheet">
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
+        <link href="../css/bootstrap-icons.css" rel="stylesheet">
 
-        <link rel="stylesheet" href="../User/css/slick.css"/>
+        <link rel="stylesheet" href="../css/slick.css"/>
 
-        <link href="../User/css/tooplate-little-fashion.css" rel="stylesheet">
+        <link href="../css/tooplate-little-fashion.css" rel="stylesheet">
         
 <!--
 
